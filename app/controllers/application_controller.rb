c5e20bebafefcfc92383462a03509f9195c6b6ba
yasmine-hj <yasminehajaji95@gmail.com>
2020-02-05 16:26:15 +0000

    refactor

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,13 @@
 class ApplicationController < ActionController::API
     before_action :authenticate_request
-     attr_reader :current_user
+    attr_reader :current_user
    
-     include ExceptionHandler
+    include ExceptionHandler
 
-     private
-   
-     def authenticate_request
-       @current_user = AuthorizeApiRequest.call(request.headers).result
-       render json: { error: 'Not Authorized' }, status: 401 unless @current_user
-     end
+    private
+    
+    def authenticate_request
+      @current_user = AuthorizeApiRequest.call(request.headers).result
+      render json: { error: 'Not Authorized' }, status: 401 unless @current_user
+    end
 end
