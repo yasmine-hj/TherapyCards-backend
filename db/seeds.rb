1e744c39866bb0619aed91c1849f9fe72484ece7
yasmine-hj <yasminehajaji95@gmail.com>
2020-02-10 16:58:34 +0000

    adding more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-topics = Topic.create([{name: 'Relationships'}, {name: 'Self'}, {name: 'Anxiety'}, {name: 'Work'}, {name: 'Anger'}])
+topics = Topic.create([{name: 'Relationships'}, {name: 'Self'}, {name: 'Anxiety'}, {name: 'Work'}, {name: 'Anger'}, {name: 'Anger'}])
 
 Card.create(question: 'How might you evolve in the way you express disappointment with a partner?', topic_id: 1)
 Card.create(question: 'What did you learn about relationships from your parents?', topic_id: 1)
@@ -18,8 +18,9 @@ Card.create(question: 'In what ways are you busy to work with, and what can you 
 Card.create(question: 'What would you like to say (but never have) to someone you have worked with?', topic_id: 4)
 Card.create(question: 'Do you have a hard time expressing feelings other than anger?', topic_id: 5)
 Card.create(question: 'Do you believe that your way is always right and get angry get angry when others disagree? (in cases where your physical/emotional wellbeing is not in danger', topic_id: 5)
+Card.create(question: 'Some people say that addiction is a disease, and others believe it is a choice. What do you think?', topic_id: 6)
+Card.create(question: 'What are some rituals or activities that you associate with your addiction that make it difficult to give up? How do you feel about giving them up?', topic_id: 6)
 
-User.create(name: 'testname', email: 'testemail@example.com', password: 'testtesttest')
 
 
 
